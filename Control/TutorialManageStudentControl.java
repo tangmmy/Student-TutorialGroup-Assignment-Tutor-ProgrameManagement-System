889d13a688a5addce1f632999553b8c3cfed0a94
tangmmy <40381217+tangmmy@users.noreply.github.com>
2023-09-10 10:54:21 +0800

    Update TutorialManageStudentControl.java

--- Control/TutorialManageStudentControl.java
+++ Control/TutorialManageStudentControl.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Control;
+package Boundary;
 
 /**
  *
@@ -10,64 +10,111 @@ package Control;
  */
 import Entity.Student;
 import Entity.TutorialGroup;
-import ADT.*;
+import Control.TutorialManageStudentControl;
 import java.util.Scanner;
-import Database.Database;
-public class TutorialManageStudentControl {
-    private TutorialGroup tutGrp;
+
+public class TutorialManageStudentUI {
+    private TutorialGroup tutorialGroup;
     private String groupName;
+    private int choice;
+    public TutorialManageStudentUI() {
+        
+    }
     
-   
-    public TutorialManageStudentControl(TutorialGroup tutGrp) {
-        this.tutGrp = tutGrp;
-    }
-
-    public TutorialManageStudentControl(TutorialGroup tutGrp,String groupName) {
-        
-        
+    public TutorialManageStudentUI(String groupName) {
+        this.tutorialGroup = new TutorialGroup();
         this.groupName = groupName;
-        Database database=new Database();
-        database.getStudentInTutorialGroup(groupName, tutGrp);
-    }
-
-    public TutorialGroup getTutorialGroup() {
-        return tutGrp;
-    }
-
-    public void setTutorialGroup(TutorialGroup tutGrp) {
-        this.tutGrp = tutGrp;
-    }
-
-    public String getGroupName() {
-        return groupName;
+        this.choice=0;
+        TutorialManageStudentControl tmsc=new TutorialManageStudentControl(tutorialGroup,groupName); 
+        
     }
 
     public void setGroupName(String groupName) {
         this.groupName = groupName;
     }
-    
-    
-    
-    
-    
-    public void addStudent(String Name,String Id){
-        Student S=new Student(Name,Id);
-        tutGrp.addStudent(S);
+    public void start(){
+        System.out.print("\nNow You Are In Manage "+this.groupName+" Page\n");
     }
-    public void deleteStudent(String Name,String Id){
-        Student S=new Student(Name,Id);
-        tutGrp.removeStudent(S);
-    }
-    public Student findStudent(String Id){
-        Student S;
-        S=tutGrp.findStudent(Id);
-        return S;
-    }
-    public void updateStudent(Student S){
+    
+    
+    public void displayOption(){
+        Scanner scan=new Scanner(System.in);
         
+        start();
+        System.out.print("\n1.Create student");
+        System.out.print("\n2.Delete student");
+        System.out.print("\n3.Find student");
+        System.out.print("\n4.Update student");
+        System.out.print("\n5.Print all students");
+        System.out.print("\n6.Exit this page");
+        System.out.print("\nEnter a selection: ");
+        this.choice=scan.nextInt();
     }
-    public void printGroup(){
-        this.tutGrp.listAllStudent();
+    public void doSomething(){
+        
+        Scanner scan=new Scanner(System.in);
+        TutorialManageStudentControl tmsc=new TutorialManageStudentControl(tutorialGroup);
+        while(this.choice!=6){
+            displayOption();
+            String name,id;
+            switch(this.choice){
+                case 1://create student
+                    
+                    
+                    //String name,id;
+                    System.out.print("\nEnter Name: ");
+                    name=scan.nextLine();
+                    System.out.print("\nEnter id: ");
+                    id=scan.nextLine();
+                    tmsc.addStudent(name,id);
+                    break;
+                
+                case 2://delete student
+                    
+                    //String name,id;
+                    System.out.print("\nEnter Name: ");
+                    name=scan.nextLine();
+                    System.out.print("\nEnter id: ");
+                    id=scan.nextLine();
+                    tmsc.deleteStudent(name,id);
+                    
+                    
+                    break;
+                    
+                
+                case 3://find student
+                    System.out.print("\nEnter id:");
+                    id=scan.nextLine();
+                    Student S=tmsc.findStudent(id);
+                    manageStud(S);
+                    
+                    break;
+                case 4:// update student
+                    System.out.print("\nNot done yet!\n");
+                    break;
+                case 5://print all students
+                    tmsc.printGroup();
+                    break;
+                        
+                
+                
+                
+            }
+            
+            
+        }   
     }
-    
+    void manageStud(Student S){
+        if(S==null){
+            System.out.print("student not found!\n");
+            return;
+        }
+        System.out.print("Now you are managing student ");
+        System.out.print("Name : "+S.getName()+"\n");
+        System.out.print("Id : "+S.getStudId()+"\n");
+        System.out.print("Tuorial Group Name: "+S.getGroupName()+"\n");
+        
+        
+        return;
+    }
 }
